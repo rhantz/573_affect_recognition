d17d2f565e81cc5938b7a939391109a8dc61696e
Elizabeth Gollhofer <elizabethgollhofer@Elizabeths-MacBook-Pro-4.local>
2023-04-16 14:36:40 -0700

    created driver, classify, create_vectors, preprocess

---
+++ src/classify.py
@@ -0,0 +1,30 @@
+from sklearn import svm
+
+
+def get_model(training_data: list, model_name: str):
+    """
+    Takes training data as input, and returns a trained sklearn model
+    Args:
+        training_data: list of lists, [0] is feature vectors and [1] is gold labels
+        model_name: string name of model type
+    Returns:
+        trained model
+    """
+    if model_name == 'svm':
+        clf = svm.SVC()
+        clf.fit(training_data[0], training_data[1])
+        return clf
+
+def train_and_classify(formatted_data: list, model_name: str):
+    """
+    Takes datasets and model name as input, returns predictions on the development data
+    Args:
+        datasets: list of lists of feature vectors and labels
+        model_name: string name of classifier model type
+    Returns:
+        predictions
+    """
+    model = get_model(formatted_data[0], model_name)
+    predictions = model.predict(formatted_data[1][0])
+    return predictions
+
---
+++ src/create_vectors.py
@@ -0,0 +1,123 @@
+import pandas as pd
+
+
+from w2v_embeddings import W2V_Embeddings, Pretrained_Embeddings
+from emo_bow import EmoBoW
+
+
+def make_vectors(args, datasets: list) -> list:
+    """
+    Given input arguments and list of datasets, creates list of vectors and gold labels
+    Args:
+        args: CLI arguments
+        datasets: list of DataFrames
+    Returns:
+         formatted data: list of lists - in each list, list[0] are feature vectors and list [1] are gold labels
+    """
+    vector_type = args.vector_type
+    if vector_type == 'pretrained':
+        model_name = args.pretrained_model
+        formatted_data = build_vectors_pretrained(datasets, model_name)
+    elif vector_type == 'w2v':
+        formatted_data = build_vectors_w2v(datasets)
+    else:
+        formatted_data = build_vectors_emobow(vector_type, datasets)
+    return formatted_data
+
+
+def build_vectors_pretrained(datasets: list, model_name: str) -> list:
+    """
+    Given input dataframe, creates vectors for the dataframes
+    Args:
+        datasets: list of DataFrames
+        model_name: string name of pretrained gensim model
+    Returns:
+         all_x_y: list of lists - in each list, list[0] are feature vectors and list [1] are gold labels
+    """
+    model = Pretrained_Embeddings(model_name)
+    all_x_y = []
+    for i in range(len(datasets)):
+        data = datasets[i]
+        essays = data['essay_processed'].values.tolist()
+        labels = data['emotion'].values.tolist()
+        vectors = []
+        for essay in essays:
+            vectors.append(model.find_centroid(essay))
+        all_x_y.append([vectors, labels])
+    return all_x_y
+
+
+def build_vectors_w2v(datasets: list) -> list:
+    """
+    Given input dataframe, creates vectors for the dataframes
+    Args:
+        datasets: list of DataFrames
+    Returns:
+         all_x_y: list of lists - in each list, list[0] are feature vectors and list [1] are gold labels
+    """
+    # First we need to train the model on the training data, and build vectors for the training data
+    train_data = datasets[0]
+    train_essays = train_data['essay_processed'].values.tolist()
+    train_labels = train_data['emotion'].values.tolist()
+    w2v = W2V_Embeddings(train_essays)
+    train_vectors = []
+    for essay in train_essays:
+        train_vectors.append(w2v.find_centroid(essay))
+    train_x_y = [train_vectors, train_labels]
+    all_x_y = [train_x_y]
+
+    # Now the model has been trained, we create vectors for the dev and/or test data
+    for i in range(1, len(datasets)):
+        data = datasets[i]
+        essays = data['essay_processed'].values.tolist()
+        labels = data['emotion'].values.tolist()
+        vectors = []
+        for essay in essays:
+            vectors.append(w2v.find_centroid(essay))
+        all_x_y.append([vectors, labels])
+    return all_x_y
+
+
+def build_vectors_emobow(vector_type: str, datasets: list):
+    """
+    Given input dataframe, creates vectors for the dataframes
+    Args:
+        vector_type: specifies plain BoW vector, emotion-only vector, or emotion enhanced BoW vector
+        datasets: list of DataFrames
+    Returns:
+         all_x_y: list of lists - in each list, list[0] are feature vectors and list [1] are gold labels
+    """
+    vocabulary = get_vocabulary(datasets[0])
+    emotion_vectorizer = EmoBoW(vocabulary, "eng")
+    all_x_y = []
+    for i in range(len(datasets)):
+        data = datasets[i]
+        word_counts = data['word_counts'].values.tolist()
+        labels = data['emotion'].values.tolist()
+        vectors = []
+        for word_count_string in word_counts:
+            if vector_type == "bow_only":
+                vectors.append(emotion_vectorizer.bow_vector(word_count_string))
+            elif vector_type == "emo_only":
+                vectors.append(emotion_vectorizer.emo_vector(word_count_string))
+            else:
+                vectors.append(emotion_vectorizer.emo_bow_vector(word_count_string))
+        all_x_y.append([vectors, labels])
+    return all_x_y
+
+
+def get_vocabulary(data: pd.DataFrame) -> set:
+    """
+    Given input dataframe, creates a list of all words in the vocabulary
+    Args:
+        data: DataFrame of training data
+    Returns:
+         vocabulary: set of all words in the vocabulary
+    """
+    vocab_set = set()
+    for row in data.iterrows():
+        text = data.loc[row[0], 'essay_processed']
+        tokens = text.split()
+        for token in tokens:
+            vocab_set.add(token)
+    return vocab_set
