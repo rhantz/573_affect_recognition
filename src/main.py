8e2ea30ec973fc975db579197bc87f4ffeea05d1
Rachel Hantz <hantz1rk@uw.edu>
2023-04-20 14:39:09 -0400

    Include ability to use config in main.py
    * add bow_dt.yml config

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """
-Driver script
-CLI usage to train and run inference with w2v:
+main script
+CLI usage to train and run inference:
+
 `python main.py
 --train_input_data_path ../data/eng/train/eng_train_new.tsv
 --dev_input_data_path ../data/eng/dev/eng_dev_combined.tsv
@@ -14,11 +15,16 @@ CLI usage to train and run inference with w2v:
 --results_dir ../results
 --score_file D2_scores.out`
 
+or
+
+`python main.py
+--config ../config/<Deliverable>/<config_name>.yml
 """
 
 import os
 import sys
 import argparse
+import yaml
 
 import preprocess
 import create_vectors
@@ -30,81 +36,98 @@ def get_args():
     """
     Returns: CLI arguments
     """
-
+    sys_args = sys.argv[1:]
     parser = argparse.ArgumentParser(description="Reads in data")
 
-    parser.add_argument(
-        "--train_input_data_path",
-        type=str,
-        required=True,
-        help="string path to initial training data from root directory",
-    )
-    parser.add_argument(
-        "--dev_input_data_path",
-        type=str,
-        required=False,
-        help="string path to initial development data from root directory",
-    )
+    if "--config" in sys_args:
 
-    parser.add_argument(
-        "--dev_gold_path",
-        type=str,
-        required=False,
-        help="string path to dev gold standard from root directory",
-    )
+        parser.add_argument(
+            "--config",
+            type=str,
+            required=False,
+            help="string path to config file with required arguments",
+        )
 
-    parser.add_argument(
-        "--test_input_data_path",
-        type=str,
-        required=False,
-        help="string path to initial test data from root directory",
-    )
+        args = parser.parse_args(sys_args)
 
-    parser.add_argument(
-        "--vector_type",
-        choices=["emo_bow", "w2v", "pretrained", "bow_only", "emo_only"],
-        type=str,
-        required=True,
-        help="vectors module to use",
-    )
+        with open(args.config, 'r') as f:
+            config = yaml.safe_load(f)
+            args.__dict__.update(config)
+    else:
 
-    parser.add_argument(
-        "--pretrained_model",
-        choices=["word2vec-google-news-300", "glove-twitter-25"],
-        type=str,
-        required=False,
-        help="name of pretrained model",
-    )
+        parser.add_argument(
+            "--train_input_data_path",
+            type=str,
+            required=True,
+            help="string path to initial training data from root directory",
+        )
+        parser.add_argument(
+            "--dev_input_data_path",
+            type=str,
+            required=False,
+            help="string path to initial development data from root directory",
+        )
 
-    parser.add_argument(
-        "--classifier",
-        choices=["svm", "dt"],
-        type=str,
-        required=True,
-        help="classifier module to use",
-    )
+        parser.add_argument(
+            "--dev_gold_path",
+            type=str,
+            required=False,
+            help="string path to dev gold standard from root directory",
+        )
 
-    parser.add_argument(
-        "--train", required=False, action="store_true", help="run training",
-    )
+        parser.add_argument(
+            "--test_input_data_path",
+            type=str,
+            required=False,
+            help="string path to initial test data from root directory",
+        )
 
-    parser.add_argument(
-        "--inf", required=False, action="store_true", help="run inference",
-    )
+        parser.add_argument(
+            "--vector_type",
+            choices=["emo_bow", "w2v", "pretrained", "bow_only", "emo_only"],
+            type=str,
+            required=True,
+            help="vectors module to use",
+        )
 
-    parser.add_argument(
-        "--output_dir", type=str, required=True, help="output directory",
-    )
+        parser.add_argument(
+            "--pretrained_model",
+            choices=["word2vec-google-news-300", "glove-twitter-25"],
+            type=str,
+            required=False,
+            help="name of pretrained model",
+        )
 
-    parser.add_argument(
-        "--results_dir", type=str, required=True, help="results directory",
-    )
+        parser.add_argument(
+            "--classifier",
+            choices=["svm", "dt"],
+            type=str,
+            required=True,
+            help="classifier module to use",
+        )
 
-    parser.add_argument(
-        "--score_file", type=str, required=True, help="name of score file",
-    )
+        parser.add_argument(
+            "--train", required=False, action="store_true", help="run training",
+        )
 
-    return parser.parse_args(sys.argv[1:])
+        parser.add_argument(
+            "--inf", required=False, action="store_true", help="run inference",
+        )
+
+        parser.add_argument(
+            "--output_dir", type=str, required=True, help="output directory",
+        )
+
+        parser.add_argument(
+            "--results_dir", type=str, required=True, help="results directory",
+        )
+
+        parser.add_argument(
+            "--score_file", type=str, required=True, help="name of score file",
+        )
+
+        args = parser.parse_args(sys_args)
+    return args
 
 
 def write_predictions(predictions):
