1e7becaa54a448be8f420ba481e7b7da33cdb397
Rachel Hantz <hantz1rk@patas.ling.washington.edu>
2023-04-22 14:55:55 -0700

    Create D2.cmd condor file to run "best" system (bow_dt)
    * move condor file to top-level
    * move run.sh to top-level and refactor accordingly
    * create D2_best_system config
    * run output and results of best system

--- D2.cmd
+++ D2.cmd
@@ -3,7 +3,7 @@ getenv     = true
 error      = foobar.error
 log        = foobar.log
 notification = complete
-arguments  = "--config ../config/D2/bow_dt.yml"
+arguments  = "--config ../config/D2/D2_best_system_bow_dt.yml"
 transfer_executable = false
 request_memory = 2*1024
 queue
