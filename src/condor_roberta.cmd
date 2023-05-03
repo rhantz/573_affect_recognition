9174e0908fdf25981160c00de326d0dd445f26ab
Elizabeth Okada <esokada@uw.edu>
2023-05-03 16:26:19 -0700

    resetting git author

---
+++ src/condor_roberta.cmd
@@ -0,0 +1,9 @@
+executable = roberta_run.sh
+getenv     = true
+error      = roberta.error
+log        = roberta.log
+notification = complete
+transfer_executable = false
+request_memory = 2*1024
+request_GPUs = 1
+queue
