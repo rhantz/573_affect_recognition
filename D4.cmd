890c6eb551850c5a99db05824e24259bc4d3c8df
Rachel Hantz <hantz1rk@patas.ling.washington.edu>
2023-05-28 06:43:45 -0700

    Add D4.cmd

---
+++ D4.cmd
@@ -0,0 +1,9 @@
+executable = run.sh
+getenv     = true
+error      = D4.error
+log        = D4.log
+notification = complete
+arguments  = "--config ../config/D4/roberta_urdu_test.yml"
+transfer_executable = false
+request_memory = 2*1024
+queue
